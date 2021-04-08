d683ea40b070988ad41e856d6e2df4cb371ea0ce
Vatslav-Sh <ride_sh@outlook.com>
2021-04-08 20:51:46 +0300

    add second method

--- 2homework4.py
+++ 2homework4.py
@@ -8,6 +8,14 @@ def _filter(i, f):
 lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 print(list(_filter(lambda x: x % 2 == 0, (iter(lst)))))
 
+# second way
+
+def _filter(lst):
+    yield [item for item in lst if item % 2 == 0]
+
+lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+result = [print(item) for item in _filter(lst)]
+
 
 
 
