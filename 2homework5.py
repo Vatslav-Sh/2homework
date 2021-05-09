a0758bcaf52582ca6d4bb513926d1748d7909729
Vatslav-Sh <ride_sh@outlook.com>
2021-05-09 21:15:38 +0300

    "update 2homework"

--- 2homework5.py
+++ 2homework5.py
@@ -1,44 +1,19 @@
 class BadClass:
 
    def __init__(self, val=0):
-       self.val = val
-       self.val2 = val
+       self.__val = val
 
-       """
-       __init__ - это в основном функция, которая будет
-       "инициализировать" / "активировать" свойства класса
-       для определенного объекта. Другое определение метода __init__ — это конструктор.
-       """
-
-
+   def get_val(self):
+        return self.__val
 
    def __hash__(self):
-       return hash(self.val2)
-
-   """
-   __hash__ Позволяет настраивать хеширование объектов
-   """
-
-
+       return hash(self.__val)
 
    def __eq__(self, other):
-       return self.val == other.val
-
-   """
-   __eq__ Позволяет реализовать проверку на равенство
-   для экземпляров пользовательских типов. object.
-   """
+       return isinstance(other, BadClass) and self.get_val() == other.__val
 
 
-
-bad = BadClass()
-
-even_worse = { bad }
-
-assert(bad in even_worse)
-
-bad.val += 1
-
-assert(bad in even_worse)
-
+bad = BadClass(val=0)
+even_worse = {bad}
+assert bad in even_worse
 print('yeah, we good!')
